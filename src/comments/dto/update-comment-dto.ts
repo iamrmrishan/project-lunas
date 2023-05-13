03dba157c6dd63134d9152962b1632b1adf8845b
iamrmrishan <iamrmrishan@gmail.com>
2023-05-13 17:02:51 +0530

    auth flow refactoring

--- src/comments/dto/update-comment-dto.ts
+++ src/comments/dto/update-comment-dto.ts
@@ -10,12 +10,14 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateComment {
   @ApiProperty()
-  @IsNotEmpty({ message: 'The Post Should Have an Id In Order To Update' })
+  @IsNotEmpty({ message: 'The Comment Should Have an Id In Order To Update' })
   @IsString()
   id: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: 'The Post Should Have an userId In Order To Update' })
+  @IsNotEmpty({
+    message: 'The Comment Should Have an userId In Order To Update',
+  })
   @IsString()
   userId: string;
 
